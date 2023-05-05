930f3bc377c01079afbb4beb89f36786d7706339
weiqiang.zhang <weiqiang.zhang@bluefocus.com>
2023-05-05 13:14:14 +0800

    fix:导出excelexcel

---
+++ test.py
@@ -0,0 +1,58 @@
+"""
+活跃账户
+"""
+from itertools import count
+from openpyxl import Workbook, load_workbook
+
+
+class ExportExcel:
+    """导出excel"""
+
+    def __init__(self, filename=None, write_only=False):
+        try:
+            if filename:
+                self.file_name = filename
+                self.wb = load_workbook(filename)
+            else:
+                self.wb = Workbook(write_only=write_only)
+        except FileNotFoundError:
+            self.wb = Workbook(write_only=write_only)
+        self.write_only = write_only
+    
+    def write_to_sheet(self, sheetname, title, data):
+        """写到一个新的sheet."""
+        ws = self.wb.create_sheet(title=sheetname)
+        ws.append(title)
+        for item in data:
+            ws.append(item)
+    
+    def append_data_to_sheet(self, sheetname, data):
+        """追加数据到一个指定sheet."""
+        ws = self.wb[sheetname]
+        ws.append(data)
+    
+    def save(self, filename):
+        """保存"""
+        self.wb.save(filename)
+        self.wb.close()
+
+    def get_sheet_data(self, sheet_name, is_title=True):
+        data = []
+        sheet = self.wb.get_sheet_by_name(sheet_name)
+
+        for index, row in zip(count(), sheet.rows):
+            if is_title and index == 0:
+                continue
+            temp_data = []
+            for cell in row:
+                temp_data.append(cell.value)
+            
+            data.append(temp_data)
+
+        return data
+        
+
+
+
+
+
